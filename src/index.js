883c50a4d97c1ad10c071f71ef65c2e744b4784c
unknown <yossef-khaled>
2020-12-13 11:05:14 +0200

    Declare who is next.

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player is: ${this.state.xIsNext ? `X` : `O`}`;
 
     return (
       <div>
