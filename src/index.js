13fe31c72367980a489bba571e4a88b4ee80a105
unknown <yossef-khaled>
2020-12-13 19:35:58 +0200

    Handle game over.

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if(calculateWinner(squares) || squares[i]) {
+      return ;
+    }
     squares[i] = this.state.xIsNext ? 'x' : 'O';
     this.setState({
       squares: squares,
@@ -36,9 +39,7 @@ class Board extends React.Component {
   }
 
   render() {
-
     const winner = calculateWinner(this.state.squares);
-    console.log(winner + " if");
     let status;
     if (winner) {
       console.log(`if condition with ${winner}`);
@@ -99,7 +100,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6]
   ];
-  
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
