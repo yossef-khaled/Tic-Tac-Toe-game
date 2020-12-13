2a7c2871467a8123ec6a05a23dd72865bfb5bdbb
unknown <yossef-khaled>
2020-12-13 09:00:55 +0200

    Delete 'this'.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 
 function Square(props) {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
+      <button className="square" onClick={() => props.onClick()}>
+        {props.value}
       </button>
     );
 }
